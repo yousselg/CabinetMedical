8785aaa40203ead8f141b4c8a5d0fa618033842a
kabarousse99 <s.kabarousse@gmail.com>
2017-11-12 14:02:59 +0100

    first commit

--- src/main/java/edu/ilisi/cabinet/controllers/DossierMedicalController.java
+++ src/main/java/edu/ilisi/cabinet/controllers/DossierMedicalController.java
@@ -1,4 +1,54 @@
 package edu.ilisi.cabinet.controllers;
 
-public class DossierMedicalControlle {
+import edu.ilisi.cabinet.model.dossiersmedicaux.DossierMedical;
+import edu.ilisi.cabinet.services.DossierMedicalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RequestMapping("/dossiermedical")
+@RestController
+@CrossOrigin
+public class DossierMedicalController {
+    @Autowired
+    private DossierMedicalService service;
+    @RequestMapping( method = RequestMethod.GET)
+    public ResponseEntity<List<DossierMedical>> listAllProducts() {
+        List<DossierMedical> dossierMedicals = (List<DossierMedical>) service.getAllDossierMedicaux();
+        if(dossierMedicals.isEmpty()){
+            return new ResponseEntity<List<DossierMedical>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
+        }
+        return new ResponseEntity<List<DossierMedical>>(dossierMedicals, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> add(@RequestBody DossierMedical input) {
+        service.addDossieMecial(input);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@RequestBody DossierMedical dossierMedical)
+    {
+        service.deleteDossierMedical(dossierMedical);
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<?> update(@RequestBody DossierMedical dossierMedical)
+    {
+        service.updateDossierMedical(dossierMedical);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @RequestMapping( value = "/dossiermedical/{id}" , method = RequestMethod.GET )
+    public ResponseEntity<?> get(@PathVariable Long id) {
+        service.getDossierMedical(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
 }
