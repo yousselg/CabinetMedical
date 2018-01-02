fcb6036666016b712f4d416d4bd8df57883f8938
yousselg <youssef.elgoud@gmail.com>
2018-01-02 20:04:38 +0000

    Commit changes

---
+++ src/test/java/edu/ilisi/cabinet/controlleurs/DossierMedicalControllerTest.java
@@ -0,0 +1,59 @@
+package edu.ilisi.cabinet.controlleurs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DossierMedicalControllerTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void testGetAllDossiersMedicaux() {
+    // fail("Not yet implemented");
+  }
+
+  @Test
+  public void testAddDossierMedical() {
+    // fail("Not yet implemented");
+  }
+
+  @Test
+  public void testDeleteDossierMedical() {
+    // fail("Not yet implemented");
+  }
+
+  @Test
+  public void testUpdateDossierMedical() {
+    // fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetDossierMedicalById() {
+    // fail("Not yet implemented");
+  }
+
+  @Test
+  public void testAddConsultationToDossierMedical() {
+    // fail("Not yet implemented");
+  }
+
+  @Test
+  public void testUpdateConsultatiobnDossierMedical() {
+    // fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetConsultationsOfDossierMedical() {
+    // fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetDetailsConsultationsOfDossierMedical() {
+    // fail("Not yet implemented");
+  }
+
+}
